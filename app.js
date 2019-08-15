c3ed536bf9a7746712e493b88d2ad977752d83af
nicholasbilly <nicholasbilly@gmail.com>
2019-08-15 15:29:08 +0700

    skeleton

---
+++ app.js
@@ -0,0 +1,33 @@
+if(process.env.NODE_ENV === 'development'){
+    require('dotenv').config()
+}
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const mongoose = require('mongoose')
+const PORT = process.env.PORT || 3000
+
+
+app.use(cors())
+app.use(express.urlencoded({extended: false}))
+app.use(express.json())
+
+mongoose.connect("mongodb://localhost:27017/group-project-week6", { useNewUrlParser: true })
+.then(success => {
+    console.log('mongoose connected')
+})
+.catch(err => {
+    console.log(err.message)
+})
+
+
+app.use((err, req, res, next) => {
+    console.log(err)
+    res.status(500).json({
+        message: err.message
+    })
+})
+
+app.listen(PORT, (req, res) => {
+    console.log(`connected to port ${PORT}`)
+})
